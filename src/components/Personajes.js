ef3537ac05ed2f268e326a46431afb9dc5983617
= <=>
2021-09-08 22:49:08 -0500

    Se realizo el taller de los personajes. Usando la serie de The Owl House

---
+++ src/components/Personajes.js
@@ -0,0 +1,23 @@
+export const personajes = [
+    {
+        nombre: 'Luz Noceda',
+        descripcion: 'Es la protagonista principal de The Owl House. Es una chica adolescente de origen dominicano-estadounidense que accidentalmente tropezó a un portal a las Islas Hirvientes, una dimensión donde la magia es real, y los humanos son tratados como seres inferiores',
+        imagen: 'https://i.pinimg.com/564x/19/df/6c/19df6c5486abc5d385e06af5986c5936.jpg'
+    },
+    {
+        nombre: 'Eda Clawthorne',
+        descripcion: 'Eda Clawthorne es la deuteragonista de The Owl House. Ella es una bruja rebelde con una recompensa de un billón de caracoles por su captura.',
+        imagen: 'https://i.pinimg.com/564x/e2/32/35/e23235447d03a816e5ec676dee893b53.jpg'
+    },
+    {
+        nombre: 'King',
+        descripcion: 'King es el tritagonista de The Owl House. Es un pequeño demonio que afirma haber sido un rey, como sugiere su nombre, pero ahora es el compañero de cuarto de Eda y busca restaurar su título y gloria.',
+        imagen: 'https://i.pinimg.com/564x/29/b6/2d/29b62dbf3062eb98a63b41dea6276521.jpg'
+    },
+    {
+        nombre: 'Amity Blight',
+        descripcion: 'Amity Blight es una joven bruja prodigiosa que asiste a la Escuela de Magia y Demonios Hexside.',
+        imagen: 'https://i.pinimg.com/564x/b7/ae/49/b7ae49076f4dbcaa27fd0c4f3d17adee.jpg'
+    }
+]
+
